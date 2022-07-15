08f2d78dc8cd1807cfeb46da4ecded2f8cf3fc01
mintertale <nekronnur@gmail.com>
2022-07-15 18:30:58 +0300

    test

--- sql_scripts/01_extraction_tables.sql
+++ sql_scripts/01_extraction_tables.sql
@@ -1,3 +1,4 @@
+BEGIN;
 create table blocks
 (
     timestamp timestamp,
@@ -161,3 +162,5 @@ create index transactions_block_timestamp_index on transactions (block_timestamp
 
 create index transactions_from_address_block_timestamp_index on transactions (from_address, block_timestamp desc);
 create index transactions_to_address_block_timestamp_index on transactions (to_address, block_timestamp desc);
+
+COMMIT;
